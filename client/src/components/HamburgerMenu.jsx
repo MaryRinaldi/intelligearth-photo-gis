eeb24483bf96109a693b83222432c057f3d2da7e
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-07-17 01:07:51 +0200

    menu on close

--- client/src/components/HamburgerMenu.jsx
+++ client/src/components/HamburgerMenu.jsx
@@ -9,6 +9,10 @@ const Hamburger = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="hamburger-menu">
       <button className="hamburger-icon" onClick={toggleMenu}>
@@ -16,10 +20,10 @@ const Hamburger = () => {
       </button>
       {isOpen && (
         <div className="menu-links">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/map" className="nav-link" >Map</Link>
-        <Link to="/gallery" className="nav-link">Gallery</Link>
-        <Link to="/profile" className="nav-link">Profile</Link>
+          <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
+          <Link to="/map" className="nav-link" onClick={closeMenu}>Map</Link>
+          <Link to="/gallery" className="nav-link" onClick={closeMenu}>Gallery</Link>
+          <Link to="/profile" className="nav-link" onClick={closeMenu}>Profile</Link>
         </div>
       )}
     </div>
