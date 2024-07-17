2288e6b0f18b3119e9c99656c867693143317bba
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-07-17 03:02:10 +0200

    check testo introduzione

--- client/src/views/FrontPage.jsx
+++ client/src/views/FrontPage.jsx
@@ -42,9 +42,9 @@ const fetchLastUploadedUrl = async () => {
       <p>
 Hai mai scattato una foto incredibile e poi hai trascorso ore a cercare di ricordare dove l'hai fatta?
 <br></br>
-Bene, è arrivato il momento di porre fine alla confusione! <br></br> Con la mia app, puoi caricare le tue foto, aggiungere titoli e descrizioni memorabili, e salvare con precisione la latitudine e la longitudine per non dimenticare mai il luogo esatto del tuo momento speciale.
+Bene, è arrivato il momento di porre fine alla confusione! <br></br> Con la mia app puoi caricare le tue foto, aggiungere titoli e descrizioni memorabili, e salvare con precisione la latitudine e la longitudine per non dimenticare mai il luogo esatto del tuo momento speciale.
 <br></br>
-Usa SNAPIFY per caricare sull'app le tue foto. Seleziona i parametri (titolo, descrizione, latitudine e longitudine), poi immetti l'URL dell'immagine che preferisci. Troverai le tue foto caricate sulla mappa (Map) e sulla galleria (Gallery) accessibili tramite il menu in alto a destra.
+Usa la sezione apposita di SNAPIFY per caricare sull'app le tue foto. Seleziona i parametri (titolo, descrizione, latitudine e longitudine), poi immetti l'URL dell'immagine che preferisci. Troverai le tue foto caricate sulla mappa (Map) e sulla galleria (Gallery) accessibili tramite il menu in alto a destra.
 <br></br>
 <br></br>
 {/* Clicca sulla mappa per impostare le tue foto alla posizione (longitude, latitude) che preferisci. Ti basterà caricare la foto dopo aver cliccato sulla mappa!
@@ -57,16 +57,16 @@ Stai cercando quella foto specifica del barbecue del 2010? Inserisci il titolo d
 <br></br>
 Esplora la sezione "Profile" per scoprire i luoghi che hai visitato e dove hai localizzato le tue foto.
 <br></br>
-E se preferisci  caricare direttamente l'immagine anziché inserire un URL, l'app gestisce tutto senza problemi; perché, diciamocelo, il Wi-Fi può essere complicato a volte.
+Presto sarà disponibile anche la funzione per  caricare direttamente l'immagine da file, anziché inserire un URL. Voglio un'app che gestisca tutto senza problemi: perché, diciamocelo, il Wi-Fi può essere complicato a volte.
 <br></br>
 <br></br>
-Grazie per aver scelto la mia app!
+Grazie per aver scelto SNAPIFY!
 <br></br>
 Spero che ti aiuti a rivivere i tuoi momenti speciali e a non perdere mai più un ricordo. <br></br>Dopotutto, la memoria è un po' come una vecchia scatola di biscotti: piena di sorprese!
 </p>
 <br></br>
       </div>
-       <p>Prova questa mappa con i click! Se invece vuoi vedere le tue foto caricate sulla mappa, vai sul Menu/Map. Lì troverai una mappa più grande.</p>
+       <p>Prova questa mappa con i click! Visualizzerai le foto inserite nella gallery. Se invece vuoi vedere le tue foto, caricate tramite l'apposita sezione sulla mappa, vai sul Menu/Map. Lì troverai una mappa più grande.</p>
       <MapComponent photos={[...uploadedPhotos, ...mockPhotos]} lastUploadedUrl={lastUploadedUrl} setLastUploadedUrl={setLastUploadedUrl} />
       <p>Queste sono le mie foto-esempio. Vai su Menu/Gallery per vedere le tue!</p>
       <div className="photo-grid">  
