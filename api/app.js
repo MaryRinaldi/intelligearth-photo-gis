efd8561019915de2d44388ff8e12aa29e53265f7
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-07-18 01:40:23 +0200

    deploy settings

--- api/app.js
+++ api/app.js
@@ -1,28 +1,20 @@
-var express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');
 const cors = require('cors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var app = express();
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
 
+const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/api', indexRouter);
-
-const corsOptions = {
-    origin: 'http://localhost:5173', 
-    optionsSuccessStatus: 200
-  };
-  app.use(cors(corsOptions));
 
 module.exports = app;
