d9d8115e9a19319d3fc26959d3ed33ee2cf3ca9e
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-07-17 11:57:02 +0200

    deploying settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['mapbox-gl'],
+    },
+  },
   server: {
     proxy: {
       "/api": {
@@ -13,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
