fc6c47f1da60c7b7fff4d2070e47164f86c63618
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-07-17 12:13:22 +0200

    public

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'public',
     rollupOptions: {
       external: ['mapbox-gl'],
     },
